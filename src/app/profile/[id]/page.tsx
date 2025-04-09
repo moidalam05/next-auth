c4458429d22124e82bc589d2c4e0fe2bc7af4290
Moid Alam <moidalam005@hotmail.com>
2025-04-09 17:30:59 +0530

    Fix user ID retrieval in UserProfile component

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -3,10 +3,9 @@ import React from "react";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const UserProfile = ({ params }: any) => {
-  const { id } = React.use(params);
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
-      <h1 className="text-3xl font-bold mb-4">User ID: {id}</h1>
+      <h1 className="text-3xl font-bold mb-4">User ID: {params.id}</h1>
       <button
         onClick={() => window.history.back()}
         className="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded"
